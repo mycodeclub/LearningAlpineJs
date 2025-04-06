ea911582fdc90a426cc4ba14985724c6ea0c427b
AnkitSahay <ankitsahay1990@gmail.com>
2025-04-06 17:23:00 +0530

    added apiDataInJson if api call break.
    move css to different file

---
+++ fallbackMenuData.js
@@ -0,0 +1,41 @@
+export const fallbackMenuData = [
+  {
+    id: 1,
+    name: "Noodels",
+    description: "Haka Noodels",
+    pricing: [{ id: 3, foodItemId: 1, quantityDesciption: "100", cost: 100 }],
+    isAvilable: true,
+    foodItemImgUrl: "/images/foodItems/1/Noodels2.jpg",
+  },
+  {
+    id: 2,
+    name: "Noodels",
+    description: "Hakka noodles are a popular Indian-Chinese dish consisting of thin, stir-fried noodles made with unleavened wheat flour, often served with vegetables and a soy sauce-based sauce.",
+    pricing: [
+      { id: 1, foodItemId: 2, quantityDesciption: "Half", cost: 70 },
+      { id: 2, foodItemId: 2, quantityDesciption: "Full", cost: 120 },
+    ],
+    isAvilable: true,
+    foodItemImgUrl: "/images/foodItems/2/Noodels.webp",
+  },
+  {
+    id: 3,
+    name: "Mango Lassi",
+    description: "Mango Lassi",
+    pricing: [{ id: 4, foodItemId: 3, quantityDesciption: "Full", cost: 100 }],
+    isAvilable: true,
+    foodItemImgUrl: "/images/foodItems/3/mangoLassi.jpg",
+  },
+  {
+    id: 4,
+    name: "Idli",
+    description: null,
+    pricing: [
+      { id: 5, foodItemId: 4, quantityDesciption: "Full -4 pice", cost: 100 },
+      { id: 6, foodItemId: 4, quantityDesciption: "Half - 2 Pice", cost: 60 },
+      { id: 7, foodItemId: 4, quantityDesciption: "Jumbo -6", cost: 140 },
+    ],
+    isAvilable: true,
+    foodItemImgUrl: "/images/foodItems/4/ChatGPT Image Apr 6, 2025, 03_33_11 PM.png",
+  },
+];
